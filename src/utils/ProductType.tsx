f1f6acfd52e275288798881c5eb6e97657361712
Saeed Moradi <ekbatan5111374@gmail.com>
2024-11-06 12:52:21 +0330

    upload-image-clear-form

--- src/utils/ProductType.tsx
+++ src/utils/ProductType.tsx
@@ -1,10 +1,10 @@
 export type product = {
-    id: number | string,
-    icon: any,
-    title: string,
-    weight: number | string,
-    Ingredients: string[],
-    price: string | number,
-    see: boolean,
-    setting: boolean
-}
+  id: number | string;
+  icon?: any;
+  title: string;
+  weight: number | string;
+  Ingredients: string[];
+  price: string | number;
+  see?: boolean;
+  setting?: boolean;
+};
